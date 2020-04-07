e5687119b068ab8a5c7407a17fcf31827b5371be
Gerardo Romero <gromero@sitek.mx>
2020-04-07 14:23:19 -0600

    Fucking sass loader version error
    "sass-loader": "~7.1.0" this need to be referenced

--- src/app/challenges/challenge-tabs/challenge-tabs.component.ts
+++ src/app/challenges/challenge-tabs/challenge-tabs.component.ts
@@ -4,29 +4,31 @@ import { RouterExtensions } from 'nativescript-angular';
 import { Page, isAndroid } from 'tns-core-modules/ui/page/page';
 
 @Component({
-  selector: 'ns-challenge-tabs',
-  templateUrl: './challenge-tabs.component.html',
-  styleUrls: ['./challenge-tabs.component.css']
+    selector: 'ns-challenge-tabs',
+    templateUrl: './challenge-tabs.component.html',
+    styleUrls: ['./challenge-tabs.component.common.css',
+        './challenge-tabs.component.css']
 })
 export class ChallengeTabsComponent implements OnInit {
 
-  constructor(private router: RouterExtensions,
-              private active: ActivatedRoute,
-              private page: Page) {
+    constructor(private router: RouterExtensions,
+        private active: ActivatedRoute,
+        private page: Page) {
 
-              }
+    }
 
 
-  ngOnInit() {
-    this.router.navigate([{
-        outlets: {
-            today: ['today'],
-            current: ['current-challenge']
-        }}],{
+    ngOnInit() {
+        this.router.navigate([{
+            outlets: {
+                today: ['today'],
+                current: ['current-challenge']
+            }
+        }], {
             relativeTo: this.active
         });
 
         this.page.actionBarHidden = true;
-  }
+    }
 
 }
